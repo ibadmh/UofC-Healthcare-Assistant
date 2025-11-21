cd124801b2fb699a8747659f4862ae9236bd195f
Ibad Hassan <ibadh145@gmail.com>
2025-11-21 16:42:19 -0700

    setting up base frontend page

---
+++ frontEnd/vite-project/src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div className="w-screen h-screen flex items-center justify-center text-gray-800">
+      <h1>UHealth Friend (Frontend Running)</h1>
+    </div>
+  );
+}
+
+
+export default App
